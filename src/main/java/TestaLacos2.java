aadfc683802f8604fdb43a11e6826da31d1bc44b
Kellyrbarros <kelly.barros@engdb.com.br>
2020-09-16 15:08:45 -0300

    Altarecao realizado no curso basico de JAVA

--- src/main/java/TestaLacos2.java
+++ src/main/java/TestaLacos2.java
@@ -1,7 +1,7 @@
 public class TestaLacos2 {
 
     public static void main(String[] args) {
-        for (int linha = 0; linha < 5; linha ++){
+        for (int linha = 0; linha < 6; linha ++){
             for (int coluna = 0; coluna < 5; coluna++){
                 if (coluna > linha){
                     break;
